5699e6a6d983741f9a2009bf96376c00dc6aad7e
Aditya Bhatia <aditya.bhatia@sap.com>
2022-12-23 10:47:13 +0100

    cleanup

--- api/v1alpha1/status.go
+++ api/v1alpha1/status.go
@@ -2,30 +2,30 @@ package v1alpha1
 
 type State string
 
-// Valid CustomObject States.
+// Valid Module CR States.
 const (
-	// StateReady signifies CustomObject is ready and has been installed successfully.
+	// StateReady signifies Module CR is Ready and has been installed successfully.
 	StateReady State = "Ready"
 
-	// StateProcessing signifies CustomObject is reconciling and is in the process of installation.
+	// StateProcessing signifies Module CR is reconciling and is in the process of installation.
 	// Processing can also signal that the Installation previously encountered an error and is now recovering.
 	StateProcessing State = "Processing"
 
-	// StateError signifies an error for CustomObject. This signifies that the Installation
+	// StateError signifies an error for Module CR. This signifies that the Installation
 	// process encountered an error.
 	// Contrary to Processing, it can be expected that this state should change on the next retry.
 	StateError State = "Error"
 
-	// StateDeleting signifies CustomObject is being deleted. This is the state that is used
+	// StateDeleting signifies Module CR is being deleted. This is the state that is used
 	// when a deletionTimestamp was detected and Finalizers are picked up.
 	StateDeleting State = "Deleting"
 )
 
 // +k8s:deepcopy-gen=true
 
-// Status defines the observed state of CustomObject.
+// Status defines the observed state of Module CR.
 type Status struct {
-	// State signifies current state of CustomObject.
+	// State signifies current state of Module CR.
 	// Value can be one of ("Ready", "Processing", "Error", "Deleting").
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Enum=Processing;Deleting;Ready;Error
