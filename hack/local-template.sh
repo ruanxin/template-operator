a0a69f0cf71bb89f9641b3f38609f0688cc2194e
Hukumraj Singh Deora <me@hukumraj-singh-deora.party>
2023-11-03 01:34:25 +0100

    fix(local-template.sh): Fix Bash Script for K3d Workaround

--- hack/local-template.sh
+++ hack/local-template.sh
@@ -3,9 +3,6 @@
 QUERY='(.spec.descriptor.component.repositoryContexts.[] | select(.).baseUrl)'
 REPO=$(yq e "$QUERY" ./template.yaml)
 # shellcheck disable=SC2001
-NEW_REPO=$(echo "$REPO" | sed -e 's/\:[0-9]*/:5000/g')
+NEW_REPO=$(echo "$REPO" | sed -E 's/:[0-9]+/:5000/g')
 echo "$NEW_REPO"
-if [[ $(yq eval .spec.target ./template.yaml) == "control-plane" ]];
-then
-        yq -i e "$QUERY |= \"$NEW_REPO\"" ./template.yaml
-fi
+yq -i e "$QUERY |= \"$NEW_REPO\"" ./template.yaml
