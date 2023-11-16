41b04e8835b01806587130af1673902eafa165a1
Benjamin Lindner <benjamin.lindner@sap.com>
2023-11-16 15:26:17 +0100

    debug echos

--- scripts/release/validate_versions.sh
+++ scripts/release/validate_versions.sh
@@ -12,12 +12,18 @@ fi
 echo "Versions match."
 
 REF_NAME="${2:-"main"}"
+echo "REF_NAME $REF_NAME"
 SHORT_EXPECTED_SHA=$(git rev-parse --short=8 "${REF_NAME}~")
+echo "SHORT_EXPECTED_SHA $SHORT_EXPECTED_SHA"
 DATE="v$(git show "${SHORT_EXPECTED_SHA}" --date=format:'%Y%m%d' --format=%ad -q)"
+echo "DATE $DATE"
 EXPECTED_VERSION="${DATE}-${SHORT_EXPECTED_SHA}"
+echo "EXPECTED_VERSION $EXPECTED_VERSION"
 
 IMAGE_TO_CHECK="${2:-europe-docker.pkg.dev/kyma-project/prod/template-operator}"
+echo "IMAGE_TO_CHECK $IMAGE_TO_CHECK"
 BUMPED_IMAGE_TAG=$(grep "${IMAGE_TO_CHECK}" ../../sec-scanners-config.yaml | cut -d : -f 2)
+echo "BUMPED_IMAGE_TAG $BUMPED_IMAGE_TAG"
 
 if [[ "$BUMPED_IMAGE_TAG" != "$EXPECTED_VERSION" ]]; then
   echo "Version tag in sec-scanners-config.yaml file is incorrect!"
