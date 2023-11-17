dde2410c738e1f350f043aae522a46300c16d48d
Benjamin Lindner <benjamin.lindner@sap.com>
2023-11-17 10:40:47 +0100

    test status main

--- scripts/release/validate_pipeline_status.sh
+++ scripts/release/validate_pipeline_status.sh
@@ -4,7 +4,7 @@ echo "Checking status of 'post-*' pipelines for template-operator"
 REF_NAME="${1:-"main"}"
 # TODO this needs to be reverted to 'kyma-project' repository after testing!
 #STATUS_URL="https://api.github.com/repos/kyma-project/template-operator/commits/${REF_NAME}/status"
-STATUS_URL="https://api.github.com/repos/lindnerby/template-operator/commits/main/status"
+STATUS_URL="https://api.github.com/repos/kyma-project/template-operator/commits/main/status"
 STATUS=$(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "${STATUS_URL}" | head -n 2 )
 if [[ "$STATUS" == *"success"* ]]; then
   echo "All recent jobs succeeded, post-pipelines are green."
