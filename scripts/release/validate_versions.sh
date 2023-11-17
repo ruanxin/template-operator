da6a899b62464094180355101f624694eab6e984
Benjamin Lindner <benjamin.lindner@sap.com>
2023-11-17 09:50:31 +0100

    fix sec-scanner-config validation

--- scripts/release/validate_versions.sh
+++ scripts/release/validate_versions.sh
@@ -13,10 +13,10 @@ echo "Versions match."
 
 IMAGE_TO_CHECK="${2:-europe-docker.pkg.dev/kyma-project/prod/template-operator}"
 BUMPED_IMAGE_TAG=$(grep "${IMAGE_TO_CHECK}" ../../sec-scanners-config.yaml | cut -d : -f 2)
-if [[ "$BUMPED_IMAGE_TAG" != "$EXPECTED_VERSION" ]]; then
+if [[ "$BUMPED_IMAGE_TAG" != "$DESIRED_VERSION" ]]; then
   echo "Version tag in sec-scanners-config.yaml file is incorrect!"
-  echo "Could not find $EXPECTED_VERSION."
+  echo "Could not find $DESIRED_VERSION."
   exit 1
 fi
-echo "Image version tag in sec-scanners-config.yaml does match with remote."
+echo "Image version tag in sec-scanners-config.yaml does match with release tag."
 exit 0
