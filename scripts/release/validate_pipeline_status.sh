aef351f23125a3dd85f025dce9c55dda747afdf6
Benjamin Lindner <benjamin.lindner@sap.com>
2023-11-17 10:20:16 +0100

    test with fork

--- scripts/release/validate_pipeline_status.sh
+++ scripts/release/validate_pipeline_status.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 echo "Checking status of 'post-*' pipelines for template-operator"
-REF_NAME=$1
-if [[ "$REF_NAME" == "" ]]; then
-  REF_NAME="main"
-fi
-echo "REF_NAME $REF_NAME"
-STATUS_URL="https://api.github.com/repos/kyma-project/template-operator/commits/${REF_NAME}/status"
+REF_NAME="${1:-"main"}"
+# TODO this needs to be reverted to main repository after testing!
+STATUS_URL="https://api.github.com/repos/lindnerby/template-operator/commits/${REF_NAME}/status"
 STATUS=$(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "${STATUS_URL}" | head -n 2 )
 if [[ "$STATUS" == *"success"* ]]; then
   echo "All recent jobs succeeded, post-pipelines are green."
