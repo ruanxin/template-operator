941f23f566fed3338a1b4d82603952da20d219d1
Xin Ruan <xin.ruan@sap.com>
2024-03-07 07:34:18 +0100

    feat: Add a new managed resources (#162)
    related issue:
    https://github.com/kyma-project/lifecycle-manager/issues/1281
    
    This PR trying to provide a new managed resource - `Managed` to template
    operator, which more aligned with most module operators who have more
    managed reosurces to deal with other than default CR.

---
+++ api/v1alpha1/managed_types.go
@@ -0,0 +1,40 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package v1alpha1 contains API Schema definitions for the component v1alpha1 API group
+// +kubebuilder:object:generate=true
+// +groupName=operator.kyma-project.io
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +kubebuilder:object:root=true
+
+type Managed struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ManagedList contains a list of Managed.
+type ManagedList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Managed `json:"items"`
+}
